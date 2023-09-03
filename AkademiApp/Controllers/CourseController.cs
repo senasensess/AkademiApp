12471c1cf3e0409e5fa740369dc13177fa68a6a9
Sena Şenses <116167054+senasensess@users.noreply.github.com>
2023-09-03 11:56:06 +0300

    Add project files.

---
+++ AkademiApp/Controllers/CourseController.cs
@@ -0,0 +1,37 @@
+﻿using AkademiApp.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AkademiApp.Controllers
+{
+    public class CourseController : Controller
+    {
+        public IActionResult Index()
+        {
+            var model = Repository.Applications;
+            return View(model);
+        }
+
+        public IActionResult Apply()//Ekranı gösterme işlemi...
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]//Modeli ekleme yapıp sonra bu işlemin gerçekleşmesini görmek amaclı home sayfasına gönderme işlemi yaptım...
+        public IActionResult Apply([FromForm] Candidate model)
+        {
+            if (Repository.Applications.Any(c => c.Email.Equals(model.Email)))
+            {
+                ModelState.AddModelError("", "There is already an applications for you.");
+            }
+            if (ModelState.IsValid)
+            {
+                Repository.Add(model);
+                return View("Feedback", model);
+            }
+            return View();
+
+        }
+    }
+}
+
---
+++ AkademiApp/Models/Repository.cs
@@ -0,0 +1,13 @@
+﻿namespace AkademiApp.Models
+{
+    public static class Repository
+    {
+        private static List<Candidate> applications = new();//Liste oluşturma işlemi yaptım...
+        public static IEnumerable<Candidate> Applications = applications;//Listeyi Çağırma işlemi yaptım...
+
+        public static void Add(Candidate candidate)
+        {
+            applications.Add(candidate);//Listeye kişi ekledim..
+        }
+    }
+}
